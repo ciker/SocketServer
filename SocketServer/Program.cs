e00220c736511d87c9e59e4db96d9f42794decf5
morteza zabihi <mz2019@gmail.com>
2018-02-22 12:41:10 +0330

    Update main

--- SocketServer/Program.cs
+++ SocketServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
@@ -10,6 +11,8 @@ namespace SocketServer
     class Program
     {
         static int serverPort = 8082;
+
+        static TaskQueue TaskQueue= new TaskQueue(new ConcurrentQueue<Task>(), 1000, 10);
 
         static IPEndPoint LocalEndPoint
         {
@@ -23,15 +26,34 @@ namespace SocketServer
 
         public static async Task Main(string[] args)
         {
+            // Subscrive to Queue Processing Events
+            TaskQueue.TaskStatus += WorkQueue_TaskStatus;
+
+            List<Task> tasks = new List<Task>(20);
+
+            //Setup Producers - To Produce Tasks and Associated Action
+            TaskProducer producerOne = new TaskProducer(TaskQueue);
+
             var listener = new TcpListener(LocalEndPoint);
             listener.Start();
 
             Console.WriteLine($"Listening on {LocalEndPoint}");
+
+            //for (int i=0; i<tasks.Count; i++)
+            //{
+            //    tasks.Add(Task.Run(() => TaskQueue.DequeueTask()));
+            //}
+
             while (true)
             {
                 var socket = await listener.AcceptTcpClientAsync();
                 await ProcessClient(socket);
             }
+        }
+
+        private static Task WorkQueue_TaskStatus(TaskProcessingArguments e)
+        {
+            return Task.FromResult(0);
         }
 
         private static async Task ProcessClient(TcpClient client)
@@ -50,6 +72,13 @@ namespace SocketServer
                     {
                         message = message + Encoding.UTF8.GetString(buffer, 0, bytesRead);
                     }
+
+                    TaskQueue.Enqueue(() =>
+                    {
+                        Console.WriteLine($"Task Dequeued: {message}");
+                    });
+
+                    Task.Run(() => TaskQueue.DequeueTask());
                 }
                 catch (Exception ex)
                 {
